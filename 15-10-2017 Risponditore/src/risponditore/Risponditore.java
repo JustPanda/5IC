19fbc75ff119b31d7e38cbf8e582d3b2c8336ebe
Manuele Lucchi <manuelelucchi@live.com>
2017-10-12 08:51:22 +0200

    Lucchi - Improved

--- 15-10-2017 Risponditore/src/risponditore/Risponditore.java
+++ 15-10-2017 Risponditore/src/risponditore/Risponditore.java
@@ -15,98 +15,98 @@ import java.util.List;
 public class Risponditore
 {
 
-    int Prezzo = 0;
-    Node Root = null;
+	int Prezzo = 0;
+	Node Root = null;
 
-    public Risponditore()
-    {
-
-    }
+	public Risponditore()
+	{
+		Root = new Node("Ciao, benvenuto al NAC", {"",""});
+	}
 }
 
 class Node
 {
 
-    public Node(String Question, List<String> Answers)
-    {
-        this.Question = Question;
-        this.Answers = Answers;
-    }
-    public String Question;
-    public List<String> Answers;
-    public List<Node> Children;
+	public Node(String Question, String[] Answers)
+	{
+		this.Question = Question;
+		this.Answers = Answers;
+	}
+	public String Question;
+	public String[] Answers;
+	public List<Node> Children;
 }
 
 class Product
 {
 
-    public Product(int Number, String Name, int Price, String Category)
-    {
-        this.Number = Number;
-        this.Name = Name;
-        this.Price = Price;
-    }
-    int Number;
-    String Name;
-    int Price;
-    String Category;
+	public Product(int Number, String Name, int Price, String Category)
+	{
+		this.Number = Number;
+		this.Name = Name;
+		this.Price = Price;
+	}
+	int Number;
+	String Name;
+	int Price;
+	String Category;
 }
 
 class Bills
 {
 
-    public Bills()
-    {
-        this.Products = new ArrayList<Product>();
-    }
-    List<Product> Products;
+	public Bills()
+	{
+		this.Products = new ArrayList<Product>();
+	}
+	List<Product> Products;
 }
 
 class Listino
 {
-    public Listino(){}
-    public String[][] Oggetti;
-    public String Categoria;
+	public Listino(String[][] Oggetti, String Categoria)
+	{
+		this.Oggetti = Oggetti;
+		this.Categoria = Categoria;
+	}
+	public String[][] Oggetti;
+	public String Categoria;
 }
 
 class ListinoTotale
 {
 
-    Listino mobili = new Listino();
-    mobili.Oggetti  =
-    {
-        {
-            ""
-        },
-        {
-            ""
-        },
-        {
-            ""
-        },
-        {
-            ""
-        },
-        {
-            ""
-        }
-    };
-    mobili.Categoria = "Mobili";
-    /*        
-    {
-        this.Oggetti  =
-        {
-            {
-            }, 
-            {
-            }, 
-            {
-            }, 
-            {
-            }, 
-            {
-            }
-        }
-    }; */
+	String[][] _mobili =
+	{
+		{
+			"Divano", "500"
+		},
+		{
+			"Letto", "1000"
+		}
+	};
+	Listino mobili = new Listino(_mobili, "Mobili");
+
+	String[][] _tappezzeria =
+	{
+		{
+			"Tappeto", "100"
+		},
+		{
+			"Moquette", "300"
+		}
+	};
+	Listino tappezzeria = new Listino(_mobili, "Tappezzeria");
+	
+	String[][] _lampadari =
+	{
+		{
+			"Lamapadario", "700"
+		},
+		{
+			"Abatjoure", "30"
+		}
+	};
+	Listino lampadari = new Listino(_mobili, "Lampadari");
 
 }
