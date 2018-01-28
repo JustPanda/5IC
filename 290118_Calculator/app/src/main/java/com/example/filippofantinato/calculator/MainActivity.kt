791d9bce59b42de7a6a7f0a80119b959d38242bb
FilippoFantinato <filippofantinato@reframe.biz>
2018-01-28 16:35:36 +0100

    Fantinato - Added calculator app

---
+++ 290118_Calculator/app/src/main/java/com/example/filippofantinato/calculator/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.filippofantinato.calculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.view.ViewCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        ViewCompat.setElevation(this, 0)
+    }
+}
